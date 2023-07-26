6d6083e15c9a4721ca32dd689d69f509227f880d
chernia222 <74971311+chernia222@users.noreply.github.com>
2023-07-26 23:06:59 +0100

    homework_3

---
+++ pythonProject3/Hillel_homework_3.py
@@ -0,0 +1,12 @@
+#Порахувати кількість унікальних символів в строці.
+# Якщо їх більше 10 - вивести в консоль True, інакше - False.
+# Строку отримати за допомогою функції input()
+first_num = input("Enter name ")
+second_num = 10
+
+length_first_num = len(first_num)
+
+if length_first_num > second_num:
+    print(True)
+else:
+    print(False)
