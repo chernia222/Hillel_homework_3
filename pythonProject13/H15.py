76e591a1ee2243905a8259b03058684bb747e2b3
chernia222 <74971311+chernia222@users.noreply.github.com>
2023-10-23 11:30:52 +0100

    Homework15

---
+++ pythonProject13/H15.py
@@ -0,0 +1,30 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+
+    return True
+
+
+def prime_numbers(start, end):
+    for num in range(start, end + 1):
+        if is_prime(num):
+            yield num
+
+
+start = 12
+end = 55
+
+print(f"Found next prime numbers from the range {{{start}:{end}}}")
+for num in prime_numbers(start, end):
+    print(num, end=", ")
+    # test
